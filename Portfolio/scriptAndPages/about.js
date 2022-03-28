eb26719db449141cdc7f4a1a2e81de5b47d513e2
Mahindra Jayavaram <95972130+Mahindra-Jayavaram@users.noreply.github.com>
2022-03-28 22:46:31 +0530

    Add files via upload

---
+++ Portfolio/scriptAndPages/about.js
@@ -0,0 +1,44 @@
+let about = document.getElementById("about")
+about.innerHTML = `
+<section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title"> ABOUT </h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="images/mahindra_profile.png" alt=""
+                        style="border-radius: 300px; background-color: rgba(146, 139, 139, 0.068);">
+                </div>
+                <div class="column right">
+                    <div class="text"> I'm Mahindra Jayavaram</div>
+                    <p>call me Mahi, </p>
+                    <p>Motivated and efficient full-stack web developer with capability to building Projects with specialization of MERN Stack. Shaped and moulded by mesmerizing Masai curriculam.
+                    <br>
+                    Eager to take a responsible role in an inspiring organization, and to deliver
+                    products that offer a Simple and Friendly user experience.
+                    </p>
+                    <br/>
+                    <div class="mediaLinksAbout">
+                         <a href="https://www.linkedin.com/in/mahindra-jayavaram-8b963a191/" target="_blank">
+                        /    <i class="fab fa-linkedin"></i>
+                        / </a>
+                        <a href="https://github.com/Mahindra-Jayavaram" target="_blank">
+                        /     <i class="fab fa-github"></i>
+                        / </a>
+                        <a href="https://medium.com/@mahindrajayavaram" target="_blank">
+                        /     <i class="fab fa-medium"></i>
+                        / </a>
+                    </div>
+                    
+                    <a href='mailto:mahindrajayavaram@gmail.com'>
+                        <p class="iconsAboutSection"><i class="fas fa-envelope"></i> mahindrajayavaram@gmail.com</p>
+                    </a>
+                    <a  href="tel:+91 6301678590">
+                        <p class="iconsAboutSection"> <i class="fas fa-phone"></i> +91-6301678590</p>
+                    </a>
+                    <a href="https://drive.google.com/file/d/1_QWqNLDjx8zzSxCMr-GWyrv5FwR-kTtg/view?usp=sharing"
+                        target="_blank" class="resumeToDrive">Resume</a>
+                </div>
+            </div>
+        </div>
+    </section>
+`;
---
+++ Portfolio/scriptAndPages/projects.js
@@ -0,0 +1,108 @@
+let projectsSection = document.getElementById("mainDivProjectsSection");
+projectsSection.innerHTML = `
+            <!-- vedix-clone 2.o -->
+            <div class="eachProjectDiv MailChimp">
+                <p class="ProjectName ">H&M India's-Clone-Backend</p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                        <img src="./images/hm2.png" alt=""/>
+                    </div>
+                    <div class="projectDescription">
+                    <p>Developed a clone of <a href="https://www2.hm.com/en_in/index.html"
+                    style="color: #FFF; text-decoration: underline;" target="_blank">H&M India's</a> website.<p >
+                        <br >
+                        Implemented maximum features of H&M India's along with a team of 5members in 6 Days.In this we add Backend to the previous project. We have made projects
+                        into Chunks and Assigned particular parts to one. And we create routers for every page. Group meets were arranged throughout the project to plan the Day accordingly.  </p>
+                        <br>
+                        <u style="font-weight:bold;">TechStack :- </u>
+                        <br>
+                        <div class="techStackProject">
+
+                            <p><i class="fab fa-js-square"></i> EJS</p>
+                            <p><i class="fab fa-css3-alt"></i> CSS</p>
+                            <p> <i class="fab fa-js-square"></i> JS </p>
+                            <p> <i class="fab fa-node-js"></i> Node.JS </p>
+                            <p> <i class="fas fa-database"></i> MongoDB </p>
+                            <p> <i class="fab fa-node-js"></i> Express </p>
+                            
+                            
+                        </div>
+                        <div class="ProjectLinks">
+                        <a href="https://github.com/Mahindra-Jayavaram/HM-Clone-Backend" target="_blank"><button>
+                        Github</button></a>
+                        <a href="https://zen-ritchie-ceb41a.netlify.app/" target="_blank"><button>Demo</button></a>
+
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <!-- vedix-clone -->
+            <div class="eachProjectDiv ">
+                <p class="ProjectName ">H&M India's - Clone</p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                        <img src="./images/hm1.png" alt="" />
+                    </div>
+                     <div class="projectDescription">
+                        <p>Developed a clone of <a href="https://www2.hm.com/en_in/index.html"
+                                style="color: #FFF; text-decoration: underline;" target="_blank">H&M India's</a> website.</p>
+                        <br >
+                        <p>It's a Collaborative project, Built by a team of 5 members  in 6 days. We have Implemented the basic 
+                        and required features of the H&M India's Website. Like, as Sign-in/Sign-UP, Products sections, Product page, Cart page, Customer Service, CheckOut page.</p>
+                        <br>
+                        <u style="font-weight:bold;">TechStack :- </u>
+                        <br>
+                        <div class="techStackProject">
+                            <p><i class="fab fa-html5"></i> HTML</p>
+                            <p><i class="fab fa-css3-alt"></i> CSS</p>
+                            <p> <i class="fab fa-js-square"></i> AdvancedJS </p>
+                            
+                        </div>
+                        <div class="ProjectLinks">
+                            <a href="https://github.com/Mahindra-Jayavaram/H-M_project"
+                                target="_blank"><button>
+                                    Github</button></a>
+                            <a href="https://zen-ritchie-ceb41a.netlify.app/" target="_blank"><button>Demo</button></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- walmart - clone -->
+            <div class="eachProjectDiv">
+                <p class="ProjectName">H&M - Clone</p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                        <img src="./images/hm3.png" alt="" />
+                    </div>
+                    <div class="projectDescription">
+                        <p>Developed a clone of <a href="https://www2.hm.com/en_in/index.html"
+                                style="color: #FFF; text-decoration: underline;" target="_blank">H&M</a> website.</p>
+                        <br >
+                        <p>H&M is an e-Commerce website. Which sells the fashion related products. It is a collabrative project with a team of 5 members in the time span of 6 days. We build the basic flow of the user experience of H&M website we also add the sorting and filter functionality.</p>
+                        </p>
+                        <br>
+                        <u style="font-weight:bold;">TechStack :- </u>
+                        <br>
+                        <div class="techStackProject">
+                            <p><i class="fab fa-html5"></i> HTML</p>
+                            <p><i class="fab fa-css3-alt"></i> CSS</p>
+                            <p> <i class="fab fa-js-square"></i> JS </p>
+                            
+                        </div>
+                        <div class="ProjectLinks">
+                            <a href="https://github.com/ThOmas-Saji/H-and-M-Clone-Project"
+                                target="_blank"><button>
+                                    Github</button></a>
+                            <a href="https://elated-volhard-275438.netlify.app/" target="_blank"><button>Demo</button></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+`;
